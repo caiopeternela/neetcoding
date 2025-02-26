51ae376f4a462808ce00240c08444b36bf3babbe
caiopeternela <caiopeternela.dev@gmail.com>
2025-02-26 11:34:53 -0300

    feat: :rocket: setting up google cloud storage

--- youtube/video-processing-service/src/index.ts
+++ youtube/video-processing-service/src/index.ts
@@ -1,28 +1,52 @@
-import express from "express";
-import ffmpeg from "fluent-ffmpeg";
+import express from "express"
+import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setupDirectories, uploadProcessedVideo } from "./storage"
+
+setupDirectories()
 
 const app = express()
 app.use(express.json())
 
-app.post("/process-video", (req, res) => {
-  // get path of the input video file from the request body
-  const inputFilePath = req.body.inputFilePath
-  const outputFilePath = req.body.outputFilePath
-
-  if (!inputFilePath || !outputFilePath) {
-    res.status(400).send("Bad Request: Missing file path.")
+app.post("/process-video", async (req, res) => {
+  // get the bucket and filename from the cloud pub/sub message
+  let data
+  try {
+    const message = Buffer.from(req.body.message.data, "base64").toString("utf-8")
+    data = JSON.parse(message)
+    if (!data.name) {
+      throw new Error("Invalid message payload received")
+    }
+  } catch (error) {
+    console.log(error)
+    return res.status(400).send("Bad Request: missing filename")
   }
 
-  ffmpeg(inputFilePath)
-    .outputOptions("-vf", "scale=-1:360")
-    .on("end", () => {
-      return res.status(200).send("Processing finished successfully")
-    })
-    .on("error", (err) => {
-      console.log(`An error occured: ${err.message}`)
-      res.status(500).send(`Interval Server Error: ${err.message}`)
-    })
-    .save(outputFilePath)
+  const inputFileName = data.name
+  const outputFileName = `processed-${inputFileName}`
+
+  // download the raw video from cloud storage
+  await downloadRawVideo(inputFileName)
+
+  // convert the video to 360p
+  try {
+    await convertVideo(inputFileName, outputFileName)
+  } catch (err) {
+    await Promise.all([
+      deleteRawVideo(inputFileName),
+      deleteProcessedVideo(outputFileName)
+    ])
+    console.log(err)
+    return res.status(500).send("Internal Server Error: video processing failed")
+  }
+
+  // upload the processed video to cloud storage
+  await uploadProcessedVideo(outputFileName)
+
+  await Promise.all([
+    deleteRawVideo(inputFileName),
+    deleteProcessedVideo(outputFileName)
+  ])
+
+  return res.status(200).send("Processing finished successfully")
 })
 
 const port = process.env.PORT || 3000
