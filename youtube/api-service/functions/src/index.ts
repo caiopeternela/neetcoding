eb2babc1e9e6c21d96702312e1c4240eda35f6bf
caiopeternela <caiopeternela.dev@gmail.com>
2025-02-28 14:33:09 -0300

    feat: :sparkles: adding the upload video function

--- youtube/api-service/functions/src/index.ts
+++ youtube/api-service/functions/src/index.ts
@@ -2,10 +2,15 @@ import * as functions from "firebase-functions/v1";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
+
+const rawVideoBucketName = "youtube-raw-videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -18,4 +23,30 @@ export const createUser = functions.auth.user().onCreate((user) => {
 
   logger.info(`User created: ${JSON.stringify(userInfo)}`);
   return;
+});
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+  // check if the user is authenticated
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated."
+    );
+  }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // generate a unique filename
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // get a v4 signed url for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return {url, fileName};
 });
