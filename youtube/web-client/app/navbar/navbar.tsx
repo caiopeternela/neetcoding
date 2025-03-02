31f2e924d5440d6ecd8393ab126ec698bd130c26
caiopeternela <caiopeternela.dev@gmail.com>
2025-03-02 08:38:02 -0300

    feat: :sparkles: cors handling & upload video feature

--- youtube/web-client/app/navbar/navbar.tsx
+++ youtube/web-client/app/navbar/navbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import Link from "next/link"
 import styles from "./navbar.module.css"
 import SignIn from "./signin"
+import Upload from "./upload"
 import { onAuthStateChangedHelper } from "../firebase/firebase"
 import { useEffect, useState } from "react"
 import { User } from "firebase/auth"
@@ -26,6 +27,9 @@ export default function Navbar() {
       <Link href="/">
         <Image width={90} height={20} src="/youtube-logo.svg" alt="YouTube Logo"/>
       </Link>
+      {
+       user &&  <Upload />
+      }
       <SignIn user={user}/>
     </nav>
   )
