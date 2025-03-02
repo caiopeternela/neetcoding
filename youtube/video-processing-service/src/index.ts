64498ae40492fd030b6bb0f178f6447368d80bd4
caiopeternela <caiopeternela.dev@gmail.com>
2025-03-02 10:12:01 -0300

    feat: :sparkles: add videos to firestore

--- youtube/video-processing-service/src/index.ts
+++ youtube/video-processing-service/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setupDirectories, uploadProcessedVideo } from "./storage"
+import { isVideoNew, setVideo } from "./firestore"
 
 setupDirectories()
 
@@ -22,6 +23,17 @@ app.post("/process-video", async (req, res): Promise<any> => {
 
   const inputFileName = data.name
   const outputFileName = `processed-${inputFileName}`
+  const videoId = inputFileName.split(".")[0]
+
+  if (!isVideoNew(videoId)) {
+    return res.status(400).send("Bad Request: video already processing or processed")
+  } else {
+    await setVideo(videoId, {
+      id: videoId,
+      uid: videoId.split("-")[0],
+      status: "processing"
+    })
+  }
 
   // download the raw video from cloud storage
   await downloadRawVideo(inputFileName)
@@ -40,6 +52,11 @@ app.post("/process-video", async (req, res): Promise<any> => {
 
   // upload the processed video to cloud storage
   await uploadProcessedVideo(outputFileName)
+
+  await setVideo(videoId, {
+    status: "processed",
+    filename: outputFileName
+  })
 
   await Promise.all([
     deleteRawVideo(inputFileName),
